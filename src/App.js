1933fd904dab32ed215ab3b39beb16da974d7571
Koji-Sunioj <koji.gabriel218@gmail.com>
2022-07-27 20:20:07 +0300

    working mvp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { API, graphqlOperation } from "aws-amplify";
+import { listTodos } from "./graphql/queries";
+import { Button, Container, Table } from "react-bootstrap";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [isError, setError] = useState(false);
+  const [isloading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    todos();
+  }, []);
+
+  const todos = async () => {
+    try {
+      const { data } = await API.graphql(graphqlOperation(listTodos));
+      setData(data.listTodos.items);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h1>Full stack React app with AWS Amplify and DynamoDB</h1>
+        {data !== null && (
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>created</th>
+                <th>name</th>
+                <th>description</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((item) => (
+                <tr key={item.id}>
+                  <td>{Date(item.createdAt)}</td>
+                  <td>{item.name}</td>
+                  <td>{item.description}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
+      </Container>
     </div>
   );
 }
