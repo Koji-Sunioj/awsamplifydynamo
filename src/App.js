460baf05844b851680f787f63cb87abe5c7cf0b0
Koji-Sunioj <koji.gabriel218@gmail.com>
2022-07-27 22:57:14 +0300

    styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { API, graphqlOperation } from "aws-amplify";
+import { API, graphqlOperation, Auth } from "aws-amplify";
 import { Authenticator, withAuthenticator } from "@aws-amplify/ui-react";
 import { listTodos } from "./graphql/queries";
 import { Button, Container, Table, Form, Col, Row } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import * as mutations from "./graphql/mutations";
+import "@aws-amplify/ui-react/styles.css";
 
 function App() {
   const [data, setData] = useState(null);
@@ -55,89 +55,99 @@ function App() {
     todos();
   };
 
+  console.log(Auth.user.attributes.email);
+
   return (
     <div className="App">
       <Container>
-        <Authenticator hideDefault={true}>
-          {({ signOut, user }) => {
-            return (
-              <main>
-                <h1>signed in as {user.attributes.email}</h1>
-                <button onClick={signOut}>Sign out</button>
-              </main>
-            );
-          }}
-        </Authenticator>
-        <h1>Full stack React app with AWS Amplify and DynamoDB!</h1>
-        {data !== null && (
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>created</th>
-                <th>name</th>
-                <th>description</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((item) => (
-                <tr key={item.id}>
-                  <td>{item.createdAt}</td>
-                  <td>{item.name}</td>
-                  <td>{item.description}</td>
-                  <td>
-                    <Button
-                      variant="danger"
-                      onClick={() => {
-                        deleteItem(item.id);
-                      }}
-                    >
-                      Delete
-                    </Button>
-                  </td>
+        <div className="section">
+          <h1>Full stack React app with AWS Amplify and DynamoDB!</h1>
+          {data !== null && (
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>created</th>
+                  <th>name</th>
+                  <th>description</th>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
-        )}
-        <h2>Create list item</h2>
-        <Form onSubmit={sendItem}>
-          <Row>
-            <Col>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>name</Form.Label>
-                <Form.Control
-                  value={name}
-                  name="itemName"
-                  placeholder="just anything tbh"
-                  onChange={(event) => {
-                    setName(event.currentTarget.value);
-                  }}
-                />
-              </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>description</Form.Label>
-                <Form.Control
-                  value={description}
-                  name="itemDescription"
-                  placeholder="Password"
-                  onChange={(event) => {
-                    setDescription(event.currentTarget.value);
-                  }}
-                />
-              </Form.Group>
-            </Col>
-          </Row>
+              </thead>
+              <tbody>
+                {data.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.createdAt}</td>
+                    <td>{item.name}</td>
+                    <td>{item.description}</td>
+                    <td>
+                      <Button
+                        variant="danger"
+                        onClick={() => {
+                          deleteItem(item.id);
+                        }}
+                      >
+                        Delete
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          )}
+        </div>
+        <div className="section">
+          <h2>Create list item</h2>
+          <Form onSubmit={sendItem}>
+            <Row>
+              <Col>
+                <Form.Group className="mb-3">
+                  <Form.Label>name</Form.Label>
+                  <Form.Control
+                    value={name}
+                    name="itemName"
+                    placeholder="just anything tbh"
+                    onChange={(event) => {
+                      setName(event.currentTarget.value);
+                    }}
+                  />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group className="mb-3">
+                  <Form.Label>description</Form.Label>
+                  <Form.Control
+                    value={description}
+                    name="itemDescription"
+                    placeholder="also anything here tbh"
+                    onChange={(event) => {
+                      setDescription(event.currentTarget.value);
+                    }}
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
 
-          <Button
-            variant="primary"
-            type="submit"
-            disabled={name.length === 0 || description.length === 0}
-          >
-            Submit
-          </Button>
-        </Form>
+            <Button
+              variant="primary"
+              type="submit"
+              disabled={name.length === 0 || description.length === 0}
+            >
+              Submit
+            </Button>
+          </Form>
+        </div>
+
+        <Authenticator></Authenticator>
+        <div className="section">
+          <span>
+            <p>signed in as {Auth.user.attributes.email}</p>{" "}
+            <Button
+              onClick={async () => {
+                Auth.signOut();
+              }}
+            >
+              sign out
+            </Button>
+          </span>
+        </div>
       </Container>
     </div>
   );
